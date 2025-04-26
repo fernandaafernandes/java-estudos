db5cd549f7462ce144652dd711a0c1269b72e5ff
Fernanda <fernandaafernandesads@gmail.com>
2025-04-26 19:26:48 -0300

    meu projeto conta banco

---
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int numero;
+        Double saldo; 
+        String agencia, nomeCliente;
+
+        Scanner sc = new Scanner(System.in);
+
+        agencia = JOptionPane.showInputDialog(null,"Por favor, digite o número da Agência");
+        sc.nextInt();
+        
+
+    //  public void statusConta() {
+
+    //  System.out.println("Digite o seu nome");
+    //  sc.nextString();
+
+    }
+}
