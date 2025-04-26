59375b161ce7ddaa5a766d8f794bc6a95a55e9df
Fernanda <fernandaafernandesads@gmail.com>
2025-04-26 20:29:56 -0300

    Projeto Conta Terminal

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,23 +1,33 @@
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+       // info
+        String nomeCliente, agencia;
         int numero;
-        Double saldo; 
-        String agencia, nomeCliente;
+        double saldo;
 
-        Scanner sc = new Scanner(System.in);
-
-        agencia = JOptionPane.showInputDialog(null,"Por favor, digite o número da Agência");
-        sc.nextInt();
+        //entrada de dados nome e agencia
+        nomeCliente = JOptionPane.showInputDialog(null, "Por favor, digite seu nome:");
+        agencia = JOptionPane.showInputDialog(null, "Por favor, digite o número da Agência:");
         
+        //entrada de dados conta e o saldo
+        String numeroStr = JOptionPane.showInputDialog(null, "Por favor, digite o número da Conta:");
+        numero = Integer.parseInt(numeroStr); 
+        
+        String saldoStr = JOptionPane.showInputDialog(null, "Por favor, digite o saldo da Conta:");
+        saldo = Double.parseDouble(saldoStr);  
 
-    //  public void statusConta() {
+        
+        mostrarStatusConta(nomeCliente, agencia, numero, saldo);
+    }
 
-    //  System.out.println("Digite o seu nome");
-    //  sc.nextString();
-
+    
+    public static void mostrarStatusConta(String nomeCliente, String agencia, int numero, double saldo) {
+        
+        JOptionPane.showMessageDialog(null, 
+            "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.\n"
+            + "Sua agência é: " + agencia + ", conta número: " + numero + "\n"
+            + "Seu saldo disponível para saque é: R$ " + saldo);
     }
 }
